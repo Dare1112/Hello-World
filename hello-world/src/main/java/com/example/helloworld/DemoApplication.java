1a5339dfacf356f4f6ea1e2a0d2356992a48d2ad
Filip Kulevski <filipkule@gmail.com>
2020-07-08 23:38:33 +0200

    Added index html as static resource. Created end points that can be pinged from index.html. Request from front end and response from back end need to be changed, beacuse they are just random

--- hello-world/src/main/java/com/example/helloworld/DemoApplication.java
+++ hello-world/src/main/java/com/example/helloworld/DemoApplication.java
@@ -2,18 +2,16 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication 
-@RestController
-public class DemoApplication { 
 
-	@GetMapping(value="/hello") 
-	public String getMethodName() { 
-		return "Hello";
-	}
+public class DemoApplication {
+	
+	//The rest controllers can be found in Controllers.java
+	//In resources/static you can find the index.html that you provided
+	//index.html is now defaultly loaded when localhost:7888 is being accessed
+	//Only thing left is to change the request and response that is being sent through the
+	//contorllers so that fits your assignment 
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
